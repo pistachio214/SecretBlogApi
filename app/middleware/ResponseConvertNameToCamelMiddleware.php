f520b6c669a46f146596447603a8772f28d19850
pengsy <songyang410@outlook.com>
2024-07-25 18:26:52 +0800

    update 添加中间件处理api返回时,小驼峰转换问题

---
+++ app/middleware/ResponseConvertNameToCamelMiddleware.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\middleware;
+
+use stdClass;
+use Webman\MiddlewareInterface;
+use Webman\Http\Response;
+use Webman\Http\Request;
+
+class ResponseConvertNameToCamelMiddleware implements MiddlewareInterface
+{
+    public function process(Request $request, callable $handler): Response
+    {
+        $response = $handler($request);
+
+        $body = json_decode($response->rawBody(), true);
+
+        $body['data'] = $this->convertToCamelCaseRecursive($body['data']);
+
+        $response->withBody(json_encode($body));
+
+        return $response;
+    }
+
+    private function convertToCamelCaseRecursive($variable): stdClass|array
+    {
+        if (is_array($variable)) {
+            $newArray = [];
+            foreach ($variable as $key => $value) {
+                $camelCaseKey = $this->toCamelCase($key);
+                if (is_array($value) || is_object($value)) {
+                    $newArray[$camelCaseKey] = $this->convertToCamelCaseRecursive($value);
+                } else {
+                    $newArray[$camelCaseKey] = $value;
+                }
+            }
+            return $newArray;
+        } elseif (is_object($variable)) {
+            $newObject = new stdClass();
+            foreach (get_object_vars($variable) as $key => $value) {
+                $camelCaseKey = $this->toCamelCase($key);
+                if (is_array($value) || is_object($value)) {
+                    $newObject->$camelCaseKey = $this->convertToCamelCaseRecursive($value);
+                } else {
+                    $newObject->$camelCaseKey = $value;
+                }
+            }
+            return $newObject;
+        } else {
+            // 如果不是数组或对象，直接返回原值
+            return $variable;
+        }
+    }
+
+    private function toCamelCase($str): string
+    {
+        // 将字符串从下划线分隔转换为小驼峰格式
+        $parts = explode('_', $str);
+        $camelCase = '';
+        foreach ($parts as $i => $part) {
+            if ($i !== 0) {
+                $part = ucfirst($part);
+            }
+            $camelCase .= $part;
+        }
+        return $camelCase;
+    }
+
+
+}
--- config/middleware.php
+++ config/middleware.php
@@ -12,4 +12,9 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-return [];
+return [
+
+    'api' => [
+        \app\middleware\ResponseConvertNameToCamelMiddleware::class,
+    ]
+];
